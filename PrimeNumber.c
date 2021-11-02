7adcb6d20fca791fcbf6cdd211751c67c6cf00d3
Mosheur Rahman <84455664+mosheur-me@users.noreply.github.com>
2021-11-02 11:41:42 +0600

    Create PrimeNumber.c

---
+++ PrimeNumber.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  printf("\t\t Welcome To Prime Number Checker\n");
+  while(1)
+  {
+    int n, i, temp;
+    
+    printf("Enter Any Number:");
+    scanf("%d",&n);
+    
+    if(n>1)
+    {
+      for(i=2;n<(i/2);i++)
+        
+    }
+    else
+    {
+      printf("Enter A Valid Number!\n");
+    }
+    getch();
+  }
+}
